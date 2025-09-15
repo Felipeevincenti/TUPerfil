6270a8649c05f174b163b0e3539dd135471da8d5
alejito120 <alejofazioestudio@gmail.com>
2025-09-15 13:31:25 -0300

    activo el modo oscuro, falta que se guarde en el localStorage y poner bien los colores

--- scripts/index.js
+++ scripts/index.js
@@ -23,23 +23,28 @@ fetch("../perfiles.json")
 
 
 function cambioTema() {
-    const temaActual = document.documentElementl.getAttribute('data-theme');
-
+    const temaActual = document.documentElement.getAttribute('data-theme');
     const temaNuevo = temaActual === 'dark' ? 'light' : 'dark';
+    aplicarTema(temaNuevo);
+    localStorage.setItem('theme', temaNuevo);
 }
 
 function aplicarTema(theme) {
     document.documentElement.setAttribute('data-theme', theme);
-
     const activador = document.getElementById('tema');
 
     if (activador) {
+
         const iconMoon = activador.querySelector('.icon-moon');
         const iconSun = activador.querySelector('.icon-sun');
+
         if (theme === 'dark') {
+
             iconMoon.style.display = 'inline';
             iconSun.style.display = 'none';
+
         }else{
+
             iconMoon.style.display = 'none';
             iconSun.style.display = 'inline';
         }
